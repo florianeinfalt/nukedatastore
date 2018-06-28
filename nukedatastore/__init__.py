cdf4f68bd47a0f7fd7e41cc94a700c8a7b2e0e7c
Florian Einfalt <florian.einfalt@me.com>
2018-06-28 15:26:27 +0100

    Version v0.2.0 release

--- nukedatastore/__init__.py
+++ nukedatastore/__init__.py
@@ -36,7 +36,7 @@ except:
         raise RuntimeError('Import nukedatastore from within Nuke')
     nuke = import_nuke()
 
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 __all__ = []
 
 DS_PREFIX = 'ds_'
